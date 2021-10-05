ce83c003202053e6ec4e0af89e275080018fe414
Bricio444 <fabriciiferreiracezario@gmail.com>
2021-10-05 19:17:07 +0000

    alteerando

--- src/main/java/application/controllers/LivroController.java
+++ src/main/java/application/controllers/LivroController.java
@@ -4,6 +4,16 @@ package application.controllers;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequestMapping("/livro")
+public class LivroController{
+    @autowired
+    private LivroRepository livrosRepo;
+    @RequstMapping("/list")
+public string list(Model model)
+model addAtribute("livro2 ", livroRepo. findAll())
+return "list. jsp";
+    }
+}
 public class LivroController {
     
 
